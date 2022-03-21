2e16838a75a05f94e6b6d2ab85d38ac14ed215aa
Alican Sungur <sunguralican@gmail.com>
2022-03-21 19:04:20 +0000

    create s3 helper module placeholder

--- src/aws/s3.js
+++ src/aws/s3.js
@@ -1,1 +1,9 @@
 // Module that talks to S3
+
+// placeholder to be updated
+const fileNameAvailable = (fileName) => {
+  console.log('You checked if a file name is available in S3');
+  return true
+}
+
+module.exports = { fileNameAvailable };
