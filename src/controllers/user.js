73e1b4d7f8f21e566a2da7f68682ed7aaac13246
Adam Trotta <ahtrotta@gmail.com>
2022-03-23 20:56:19 -0400

    added logic to user controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,44 +1,76 @@
 const User = require('../models/User');
+const { hashPassword } = require('../utils/userAuthentication');
 
 const retrieveAll = async (req, res, next) => {
   // Get all users
   // admin-app
-  const users = await User.find({});
-  res.json(users);
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
-const retrieve = (req, res, next) => {
+const retrieve = async (req, res, next) => {
   // Get a single user
   // admin-app
   const userId = req.params.id;
-  res.status(200).json({ message: `You get a single user. Id was: ${userId}`});
+  try {
+    const user = await User.findById(userId);
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
-const create = (req, res, next) => {
+const create = async (req, res, next) => {
   // Create a single user
   // Both client-sdk and admin-app
-  const user = req.body?.user;
-
-  res.status(201).json({ message: `You created a user ${user ? 'with' : 'without'} a request body`});
+  try {
+    const hashedPassword = await hashPassword(req.body.password);
+    const user = {
+      ...req.body,
+      password: hashedPassword
+    };
+    const response = await User.create(user)
+    res.status(201).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
-const update = (req, res, next) => {
+const update = async (req, res, next) => {
   // Update a user
   // Both client-sdk and admin-app
-  const user = req.body?.user;
-
-  res.status(201).json({ message: `You updated a user ${user ? 'with' : 'without'} a request body`});
+  const userId = req.params.id;
+  try {
+    const hashedPassword = await hashPassword(req.body.password);
+    const user = {
+      ...req.body,
+      password: hashedPassword
+    };
+    const response = await User.replaceOne({ _id: userId }, user);
+    res.status(201).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 };
 
-const remove = (req, res, next) => {
+const remove = async (req, res, next) => {
   // Delete a user
   // Both client-sdk and admin-app
-  const id = req.params.id;
-  console.log(id);
-  if (id) {
-    res.status(204).send();
-  } else {
-    res.status(404).json({ message: "You haven't passed an id"});
+  const userId = req.params.id;
+  try {
+    const response = await User.findByIdAndDelete(userId);
+    res.status(204).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
   }
 };
 
